32cee99b0c7a28343c74fab0c28dae2813df44ec
aming <z5866318@gmail.com>
2026-03-25 22:15:45 -0400

    [checkpoint] auto-save before task test-1774491341-2d1990

--- agent/utils.py
+++ agent/utils.py
@@ -40,6 +40,7 @@ def utc_ts_ms() -> int:
 
 
 def utc_iso() -> str:
+    """Return the current UTC time as an ISO 8601 formatted string."""
     return time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
 
 
